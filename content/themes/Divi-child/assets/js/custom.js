4d808cdf2215a2a2f6aab4a3172fdfb7395e7fbb
John Fieldsend <jfieldsend@gmail.com>
2018-01-25 16:52:57 +0000

    no message

---
+++ content/themes/Divi-child/assets/js/custom.js
@@ -0,0 +1,207 @@
+(function ($) {
+  "use strict";
+
+  function logo_swap() {
+    $("#main-header").each(function() {
+      var src = $('#logo').attr('src');
+      if ($(this).hasClass('et-fixed-header')) {
+        src = src.replace("[white]", "[blue]");
+        $("#logo").attr('src', src);
+      } else {
+        src = src.replace("[blue]", "[white]");
+        $("#logo").attr('src', src);
+      }
+    });
+  }
+
+  function page_banner() {
+    $("#banner_img").css('background-image', function () {
+      var bg = ('url(' + $(this).data("image-src") + ')');
+      return bg;
+    });
+    var imgH = $('#heroImg').height();
+    $('#banner_curve').css("padding-bottom", imgH+"px");
+  }
+
+  function past_banner() {
+    if($(document).scrollTop()>=$('#main-content').position().top){
+      $('#main-header').toggleClass('past');
+    }
+  }
+
+  function popupgallery() {
+    $('.popup-gallery').magnificPopup({
+      delegate: 'a.popup-img',
+      type: 'image',
+      tLoading: 'Loading image #%curr%...',
+      mainClass: 'mfp-img-mobile',
+      gallery: {
+        enabled: true,
+        navigateByImgClick: true,
+        preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+      },
+      image: {
+        tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+        titleSrc: function(item) {
+          return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+        }
+      }
+    });
+
+    $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+      disableOn: 700,
+      type: 'iframe',
+      mainClass: 'mfp-fade',
+      removalDelay: 160,
+      preloader: false,
+      fixedContentPos: false
+    });
+  }
+
+  function screensilder() {
+      var slide = $('.slider-single');
+      var slideTotal = slide.length - 1;
+      var slideCurrent = -1;
+
+      function slideInitial() {
+          slide.addClass('proactivede');
+          setTimeout(function() {
+              slideRight();
+          }, 500);
+      }
+
+      function slideRight() {
+          if (slideCurrent < slideTotal) {
+              slideCurrent++;
+          } else {
+              slideCurrent = 0;
+          }
+
+          if (slideCurrent > 0) {
+              var preactiveSlide = slide.eq(slideCurrent - 1);
+          } else {
+              var preactiveSlide = slide.eq(slideTotal);
+          }
+          var activeSlide = slide.eq(slideCurrent);
+          if (slideCurrent < slideTotal) {
+              var proactiveSlide = slide.eq(slideCurrent + 1);
+          } else {
+              var proactiveSlide = slide.eq(0);
+
+          }
+
+          slide.each(function() {
+              var thisSlide = $(this);
+              if (thisSlide.hasClass('preactivede')) {
+                  thisSlide.removeClass('preactivede preactive active proactive').addClass('proactivede');
+              }
+              if (thisSlide.hasClass('preactive')) {
+                  thisSlide.removeClass('preactive active proactive proactivede').addClass('preactivede');
+              }
+          });
+          preactiveSlide.removeClass('preactivede active proactive proactivede').addClass('preactive');
+          activeSlide.removeClass('preactivede preactive proactive proactivede').addClass('active');
+          proactiveSlide.removeClass('preactivede preactive active proactivede').addClass('proactive');
+      }
+
+      function slideLeft() {
+          if (slideCurrent > 0) {
+              slideCurrent--;
+          } else {
+              slideCurrent = slideTotal;
+          }
+
+          if (slideCurrent < slideTotal) {
+              var proactiveSlide = slide.eq(slideCurrent + 1);
+          } else {
+              var proactiveSlide = slide.eq(0);
+          }
+          var activeSlide = slide.eq(slideCurrent);
+          if (slideCurrent > 0) {
+              var preactiveSlide = slide.eq(slideCurrent - 1);
+          } else {
+              var preactiveSlide = slide.eq(slideTotal);
+          }
+          slide.each(function() {
+              var thisSlide = $(this);
+              if (thisSlide.hasClass('proactivede')) {
+                  thisSlide.removeClass('preactive active proactive proactivede').addClass('preactivede');
+              }
+              if (thisSlide.hasClass('proactive')) {
+                  thisSlide.removeClass('preactivede preactive active proactive').addClass('proactivede');
+              }
+          });
+          preactiveSlide.removeClass('preactivede active proactive proactivede').addClass('preactive');
+          activeSlide.removeClass('preactivede preactive proactive proactivede').addClass('active');
+          proactiveSlide.removeClass('preactivede preactive active proactivede').addClass('proactive');
+      }
+
+      var left = $('.slider-left');
+      var right = $('.slider-right');
+      left.on('click', function() {
+          slideLeft();
+      });
+      right.on('click', function() {
+          slideRight();
+      });
+      slideInitial();
+  }
+
+  // function price_toggle() {
+  function changeCurrency(flag) {
+    var val = flag.val();
+    var flagDefault = $('.edition_price').data('currency');
+    if (val != flagDefault) {
+      $('.edition_price').data('currency', val);
+      flag.addClass('selected');
+    }
+    updatePrice();
+  }
+
+  function changeSubscription(sub) {
+    var subDefault = $('.edition_price').data('subscription');
+    if (sub != subDefault) {
+      $('.edition_price').data('subscription', sub);
+    }
+    updatePrice();
+  }
+
+  function updatePrice() {
+    var price = $('.edition_price'),
+      sub = $('.edition_price').data('subscription'),
+      cur = $('.edition_price').data('currency'),
+      tag = (sub == 'monthly') ? '<small> /monthly</small>' : '<small> /annualy</small>';
+    price.each(function() {
+      $(this).html($(this).data(sub+'-'+cur)+tag);
+    });
+  }
+
+  /*************************
+  All function are called here either on page load or scroll
+  *************************/
+  $(document).ready(function() {
+    $('.edition_price').data({subscription:'monthly',  currency:'usd'});
+    page_banner();
+    logo_swap();
+    screensilder();
+    popupgallery();
+    updatePrice();
+    $('.currency').change(function() { changeCurrency($(this)) });
+    $('#priceToggle').change(function() {
+      if (this.checked) {
+        changeSubscription('annualy');
+      } else {
+        changeSubscription('monthly');
+      }
+    });
+  });
+
+  $(document).on('scroll', function() {
+    logo_swap();
+  });
+
+  $(window).on('resize', function() {
+    page_banner();
+  });
+
+})(jQuery);
