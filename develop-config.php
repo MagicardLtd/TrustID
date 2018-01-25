bb4bd711e2494964738627967d18858754d28217
John Fieldsend <jfieldsend@gmail.com>
2018-01-25 20:05:37 +0000

    Dev config

---
+++ develop-config.php
@@ -0,0 +1,8 @@
+<?php
+define('DB_NAME',     'dev_trustidsoft');
+define('DB_USER',     'trust_id');
+define('DB_PASSWORD', 'Gre549v?');
+define('DB_HOST',     'localhost');
+define('DB_CHARSET',  'utf8');
+define('DB_COLLATE',  '');
+?>
