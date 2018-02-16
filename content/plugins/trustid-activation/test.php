9fcd019c9171177fa35c191e6b4072e56b181fbe
John Fieldsend <jfieldsend@gmail.com>
2018-02-16 13:01:02 +0000

    no message

--- content/plugins/trustid-activation/test.php
+++ content/plugins/trustid-activation/test.php
@@ -1,16 +1,16 @@
 <?php
-  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (!isset($bypassPostCheck)) {
-      tid_process_request();
-    }
-  } else if (!isset($bypassPostCheck)) {
-    header('HTTP/1.0 405 Method Not Allowed');
-    header('Allow: POST');
-    header('Content-type: text/html');
-    echo '<!DOCTYPE html><html><body><p>405 Method Not Allowed</p></body></html>';
-  }
-
-  function tid_process_request($web = false) {
+  // if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  //   if (!isset($bypassPostCheck)) {
+  //     tid_process_request();
+  //   }
+  // } else if (!isset($bypassPostCheck)) {
+  //   header('HTTP/1.0 405 Method Not Allowed');
+  //   header('Allow: POST');
+  //   header('Content-type: text/html');
+  //   echo '<!DOCTYPE html><html><body><p>405 Method Not Allowed</p></body></html>';
+  // }
+  //
+  // function tid_process_request($web = false) {
 
     $requestCode = $_POST['RequestCode'];
     require_once dirname(__FILE__).'/trustid-activation-request-response.php';
@@ -31,6 +31,6 @@
       echo 'Email="' . $email . '" ';
     }
     echo 'Status="' . $status . '" />';
-  }
+  // }
 
 ?>
